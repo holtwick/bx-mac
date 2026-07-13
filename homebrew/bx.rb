aacce950488aae45c10bcd7cf2e8e4863b034567
Dirk Holtwick <dirk.holtwick@gmail.com>
2026-07-13 09:55:19 +0200

    1.8

--- homebrew/bx.rb
+++ homebrew/bx.rb
@@ -1,8 +1,8 @@
 class Bx < Formula
   desc "Launch apps in a macOS sandbox with only the project directory accessible"
   homepage "https://github.com/holtwick/bx-mac"
-  url "https://registry.npmjs.org/bx-mac/-/bx-mac-1.7.3.tgz"
-  sha256 "cfa6ae727ba42f3e3cc1f882dcae5afdaa87b02b354559ced6021afce4c2241e"
+  url "https://registry.npmjs.org/bx-mac/-/bx-mac-1.8.0.tgz"
+  sha256 "d4f35bc7035b1d3509983bbf4c27c38da88481e6a3c308ba3181f30ebd319184"
   license "MIT"
 
   depends_on :macos
