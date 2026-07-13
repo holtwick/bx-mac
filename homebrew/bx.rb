6aa7f03697a753d5f9114f8a05fd1417efc16aa6
Dirk Holtwick <dirk.holtwick@gmail.com>
2026-07-13 09:36:59 +0200

    prepare homebrew release

---
+++ homebrew/bx.rb
@@ -0,0 +1,19 @@
+class Bx < Formula
+  desc "Launch apps in a macOS sandbox with only the project directory accessible"
+  homepage "https://github.com/holtwick/bx-mac"
+  url "https://registry.npmjs.org/bx-mac/-/bx-mac-1.7.3.tgz"
+  sha256 "cfa6ae727ba42f3e3cc1f882dcae5afdaa87b02b354559ced6021afce4c2241e"
+  license "MIT"
+
+  depends_on :macos
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bx --version")
+  end
+end
